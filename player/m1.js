54d003fb9c9e7e8a3d0d5fdb7168630dfa1c44f3
topanekka <126932631+topanekka@users.noreply.github.com>
2023-12-07 18:47:14 +0530

    Add files via upload

---
+++ player/m1.js
@@ -0,0 +1,28 @@
+   
+        jwplayer("myElement").setup({
+           autostart: true,
+           "playbackRateControls": true,
+          "playbackRateControls": [0.25, 0.5, 0.75, 1, 1.25, 1.5, 2,2.25,2.5,3,4],
+           "preload": "metadata",
+  "repeat": true,
+  "volume": 100,
+  "mute": false,
+  "unmute": true,
+     "volume": "100",
+  "mute": "false",
+    "unmute": "true",
+  "stretching": "exactfit",
+  "width": "100%",
+   "height": "100%",
+    
+			          "setFullscreen": true,
+			       
+              "cast": {},
+                    "image": 'https://i.imgur.com/hjPt9cu.jpg',
+			          autostart: true,
+			          primary: 'html5',
+			          setFullscreen: true,
+			          preload: 'metadata',
+			          mute: 'false',
+     
+        });
